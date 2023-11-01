db5c19a4ff7c79d5a78db24d75a9e5dcb8c7b965
dan-peveto <daniel_peveto@outlook.com>
2023-11-01 13:50:02 -0700

    first commit

---
+++ FavoriteFruit.java
@@ -0,0 +1,9 @@
+/**
+ * FavoriteFruit
+ */
+public class FavoriteFruit {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test of your favorite fruit");
+    }
+}
